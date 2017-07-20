66391640b2b4985a1da4f8bfd23ab135b611b428
Collins Mwiti <mwiticmutumamwenda@gmail.com>
2017-07-20 09:08:00 +0300

    connected to my blog

--- js/custom.js
+++ js/custom.js
@@ -1,63 +1,62 @@
-$(window).load(function () {
+$(window).load(function() {
 
-    // preloader
-    $('#status').fadeOut(); // will first fade out the loading animation
-    $('#preloader').delay(550).fadeOut('slow'); // will fade out the white DIV that covers the website.
-    $('body').delay(550).css({
-        'overflow': 'visible'
-    });
+  // preloader
+  $('#status').fadeOut(); // will first fade out the loading animation
+  $('#preloader').delay(550).fadeOut('slow'); // will fade out the white DIV that covers the website.
+  $('body').delay(550).css({
+    'overflow': 'visible'
+  });
 
+  //  isotope
+  var $container = $('.portfolio_container');
+  $container.isotope({
+    filter: '*',
+  });
 
-    //  isotope
-    var $container = $('.portfolio_container');
+  $('.portfolio_filter a').click(function() {
+    $('.portfolio_filter .active').removeClass('active');
+    $(this).addClass('active');
+
+    var selector = $(this).attr('data-filter');
     $container.isotope({
-        filter: '*',
+      filter: selector,
+      animationOptions: {
+        duration: 500,
+        animationEngine: "jquery"
+      }
     });
+    return false;
+  });
 
-    $('.portfolio_filter a').click(function () {
-        $('.portfolio_filter .active').removeClass('active');
-        $(this).addClass('active');
+  // back to top
+  var offset = 300,
+    offset_opacity = 1200,
+    scroll_top_duration = 700,
+    $back_to_top = $('.cd-top');
 
-        var selector = $(this).attr('data-filter');
-        $container.isotope({
-            filter: selector,
-            animationOptions: {
-                duration: 500,
-                animationEngine: "jquery"
-            }
-        });
-        return false;
-    });
+  //hide or show the "back to top" link
+  $(window).scroll(function() {
+    ($(this).scrollTop() > offset) ? $back_to_top.addClass('cd-is-visible'): $back_to_top.removeClass('cd-is-visible cd-fade-out');
+    if ($(this).scrollTop() > offset_opacity) {
+      $back_to_top.addClass('cd-fade-out');
+    }
+  });
 
-    // back to top
-    var offset = 300,
-        offset_opacity = 1200,
-        scroll_top_duration = 700,
-        $back_to_top = $('.cd-top');
+  //smooth scroll to top
+  $back_to_top.on('click', function(event) {
+    event.preventDefault();
+    $('body,html').animate({
+      scrollTop: 0,
+    }, scroll_top_duration);
+  });
 
-    //hide or show the "back to top" link
-    $(window).scroll(function () {
-        ($(this).scrollTop() > offset) ? $back_to_top.addClass('cd-is-visible'): $back_to_top.removeClass('cd-is-visible cd-fade-out');
-        if ($(this).scrollTop() > offset_opacity) {
-            $back_to_top.addClass('cd-fade-out');
-        }
-    });
-
-    //smooth scroll to top
-    $back_to_top.on('click', function (event) {
-        event.preventDefault();
-        $('body,html').animate({
-            scrollTop: 0,
-        }, scroll_top_duration);
-    });
-
-    // input
-    $(".input-contact input, .textarea-contact textarea").focus(function () {
-        $(this).next("span").addClass("active");
-    });
-    $(".input-contact input, .textarea-contact textarea").blur(function () {
-        if ($(this).val() === "") {
-            $(this).next("span").removeClass("active");
-        }
-    });
+  // input
+  $(".input-contact input, .textarea-contact textarea").focus(function() {
+    $(this).next("span").addClass("active");
+  });
+  $(".input-contact input, .textarea-contact textarea").blur(function() {
+    if ($(this).val() === "") {
+      $(this).next("span").removeClass("active");
+    }
+  });
 });
